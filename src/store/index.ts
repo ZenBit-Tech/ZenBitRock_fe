a02b26ac67022ac970a4a9c99b9176654be40aea
AKutusenko <76035623+AKutusenko@users.noreply.github.com>
2023-11-06 15:50:13 +0100

    fix: configurates store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/dist/query';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import counterReducer from './reducers/testReducer';
 
+const persistConfig = {
+  key: 'store',
+  whitelist: [],
+  storage,
+};
+
+const reducers = combineReducers({
+  counter: counterReducer
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+      immutableCheck: false,
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-
 export type AppDispatch = typeof store.dispatch;
+setupListeners(store.dispatch);
