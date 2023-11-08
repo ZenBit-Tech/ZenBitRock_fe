3c96c0d8124e8a61567a3d67d32dc6dda0ac336c
AKutusenko <76035623+AKutusenko@users.noreply.github.com>
2023-11-08 14:53:41 +0100

    fix: optimizes styles

--- src/app/[lang]/testpage/components/reduxExample/styles.ts
+++ src/app/[lang]/testpage/components/reduxExample/styles.ts
@@ -1,14 +1,16 @@
-import { styled } from '@mui/material';
+'use client';
+
+import styled from 'styled-components';
 import { colors } from 'constants/colors';
 
-export const ReduxWrapper = styled('div')`
+export const ReduxWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 150px;
 `;
 
-export const Button = styled('button')`
+export const Button = styled.button`
   background: ${colors.TEST_BACKGROUND_COLOR};
   color: ${colors.TEST_MAIN_COLOR};
   font-size: 1em;
