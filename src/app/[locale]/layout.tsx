895b9a44d093a3fe71ee431fcff31ae9474545c4
AKutusenko <76035623+AKutusenko@users.noreply.github.com>
2023-11-11 21:44:08 +0100

    feature: adds localization provider for react-hook-form

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,6 +3,8 @@ import 'modern-normalize/modern-normalize.css';
 import StyledComponentsRegistry from 'lib/registry';
 import { Locale, generateStaticParams } from 'locales/i18n.config';
 import TestHeader from 'components/Test-Header';
+import ThemeProvider from 'theme';
+import { LocalizationProvider } from 'locales';
 
 export const metadata = {
   title: 'Agent wise',
@@ -26,10 +28,14 @@ export default async function RootLayout({ children, params }: Props) {
   return (
     <html lang={params.locale}>
       <body>
-        <StyledComponentsRegistry>
-          <TestHeader locale={params.locale} />
-          {children}
-        </StyledComponentsRegistry>
+        <LocalizationProvider>
+          <ThemeProvider>
+            <StyledComponentsRegistry>
+              <TestHeader locale={params.locale} />
+              {children}
+            </StyledComponentsRegistry>
+          </ThemeProvider>
+        </LocalizationProvider>
       </body>
     </html>
   );
