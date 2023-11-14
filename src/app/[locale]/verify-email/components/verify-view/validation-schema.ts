49bfb8edf395cc0159693914eb0676f6876aff18
canterbery <wotphantommail@gmail.com>
2023-11-14 18:08:47 +0200

    feat: + code length

--- src/app/[locale]/verify-email/components/verify-view/validation-schema.ts
+++ src/app/[locale]/verify-email/components/verify-view/validation-schema.ts
@@ -1,7 +1,11 @@
 import * as Yup from 'yup';
 
+const CODE_LENGTH = 6;
+
 const VerifySchema = Yup.object().shape({
-  code: Yup.string().min(6, 'Code must be at least 6 characters').required('Code is required'),
+  code: Yup.string()
+    .min(CODE_LENGTH, `Code must be at least ${CODE_LENGTH} characters`)
+    .required('Code is required'),
   email: Yup.string().required('Email is required').email('Email must be a valid email address'),
 });
 
