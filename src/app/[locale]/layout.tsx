814416ccf62e376a0701041db68b379990dbce4c
canterbery <wotphantommail@gmail.com>
2023-11-14 18:00:44 +0200

    feat: + template settings and localization

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,8 +1,14 @@
+// i18n
+import 'locales/i18n';
+
 import './global.css';
 import 'modern-normalize/modern-normalize.css';
 import StyledComponentsRegistry from 'lib/registry';
 import { Locale, generateStaticParams } from 'locales/i18n.config';
 import TestHeader from 'components/Test-Header';
+import { LocalizationProvider } from 'locales';
+import { SettingsProvider, SettingsValueProps } from 'components/settings';
+import ThemeProvider from 'theme';
 
 export const metadata = {
   title: 'Agent wise',
@@ -20,16 +26,31 @@ type Props = {
   params: { locale: Locale };
 };
 
+const defaultSettings: SettingsValueProps = {
+  themeMode: 'light', // 'light' | 'dark'
+  themeDirection: 'ltr', //  'rtl' | 'ltr'
+  themeContrast: 'default', // 'default' | 'bold'
+  themeLayout: 'vertical', // 'vertical' | 'horizontal' | 'mini'
+  themeColorPresets: 'default', // 'default' | 'cyan' | 'purple' | 'blue' | 'orange' | 'red'
+  themeStretch: false,
+};
+
 generateStaticParams();
 
-export default async function RootLayout({ children, params }: Props) {
+export default function RootLayout({ children, params }: Props) {
   return (
     <html lang={params.locale}>
       <body>
-        <StyledComponentsRegistry>
-          <TestHeader locale={params.locale} />
-          {children}
-        </StyledComponentsRegistry>
+        <LocalizationProvider>
+          <SettingsProvider defaultSettings={defaultSettings}>
+            <ThemeProvider>
+              <StyledComponentsRegistry>
+                <TestHeader locale={params.locale} />
+                {children}
+              </StyledComponentsRegistry>
+            </ThemeProvider>
+          </SettingsProvider>
+        </LocalizationProvider>
       </body>
     </html>
   );
