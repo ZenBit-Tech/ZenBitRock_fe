32817a3ab70429768105fab53ad3c5b4bec15a85
Zhenkaaf <zhenkaaf@gmail.com>
2023-11-15 12:18:14 +0200

    upd

--- src/app/[lang]/signinpage/styles.ts
+++ src/app/[lang]/signinpage/styles.ts
@@ -11,7 +11,7 @@ export const Wrapper = styled('div')`
 
 export const LeftSection = styled('div')`
   flex: 2;
-  background-image: url('https://w.forfun.com/fetch/6c/6c0cf748db116fabaa70199a35327258.jpeg');
+  background-image: url(${colors.BG_LOGIN_PAGE});
   background-size: cover;
   background-position: center;
   display: flex;
--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,4 +1,5 @@
 export const colors = {
   TEST_BACKGROUND_COLOR: '#172234',
   TEST_MAIN_COLOR: '#808080',
+  BG_LOGIN_PAGE: 'https://w.forfun.com/fetch/6c/6c0cf748db116fabaa70199a35327258.jpeg',
 };
