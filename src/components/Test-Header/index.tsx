027b9d1556650f05d6319037039087542cd95011
Hanna <anna_kon@bigmir.net>
2023-11-15 15:19:35 +0200

    feat(signup): implement sign-up page functionality, validation, i18n

--- src/components/Test-Header/index.tsx
+++ src/components/Test-Header/index.tsx
@@ -1,18 +1,23 @@
-import { Logo, TestHeaderItem } from './styles';
+import { notFound } from 'next/navigation';
 import { Locale } from 'locales/i18n.config';
 import { getDictionary } from 'lib/dictionary';
 import LocaleSwitcher from 'components/locale-switcher';
+import { Logo, TestHeaderItem } from './styles';
 
 type Props = {
   lang: Locale;
 };
 
 export default async function TestHeader({ lang }: Props) {
-  const { Home } = await getDictionary(lang);
-  return (
-    <TestHeaderItem>
-      <Logo href="/">{Home.TestHeader.title}</Logo>
-      <LocaleSwitcher />
-    </TestHeaderItem>
-  );
+  try {
+    const { Home } = await getDictionary(lang);
+    return (
+      <TestHeaderItem>
+        <Logo href="/">{Home.Header.title}</Logo>
+        <LocaleSwitcher />
+      </TestHeaderItem>
+    );
+  } catch (error) {
+    notFound();
+  }
 }
