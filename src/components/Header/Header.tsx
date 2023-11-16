4c2f166de17bfe218d6f912e131d97326686d65d
Hanna <anna_kon@bigmir.net>
2023-11-16 22:37:43 +0200

    fix: Header code refactoring

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,44 @@
-'use client'
-import { Logo, HeaderItem } from './styles';
+'use client';
+
+import { useEffect, useState } from 'react';
+import { notFound } from 'next/navigation';
 import { Locale } from 'locales/i18n.config';
 import { getDictionary } from 'lib/dictionary';
 import LocaleSwitcher from 'components/locale-switcher';
+import { pageLinks } from 'constants/pageLinks';
+import { Logo, HeaderItem } from './styles';
 
 type Props = {
   lang: Locale;
 };
 
-export default async function Header({ lang }: Props) {
-  const { Home } = await getDictionary(lang);
+type HeaderType = {
+  Header: {
+    [key: string]: string;
+  };
+  Page: {
+    [key: string]: string;
+  };
+};
+
+export default function Header({ lang }: Props) {
+  const [data, setData] = useState<HeaderType | null>(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { Home } = await getDictionary(lang);
+        setData(Home);
+      } catch (error) {
+        notFound();
+      }
+    };
+
+    fetchData();
+  }, [lang]);
+
   return (
     <HeaderItem>
-      <Logo href="/">{Home.Header.title}</Logo>
+      <Logo href={pageLinks.HOME_PAGE}>{data?.Header.title}</Logo>
       <LocaleSwitcher />
     </HeaderItem>
   );
