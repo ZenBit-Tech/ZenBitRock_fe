9f25c55edcf032046c9a4dc5baa51ef4d128cbfb
AKutusenko <76035623+AKutusenko@users.noreply.github.com>
2023-11-18 22:33:42 +0100

    fix: deleted test home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,3 @@
-'use client';
-
-import { useTranslations } from 'next-intl';
-import { Link, Wrapper } from './styles';
-
 export default function HomePage(): JSX.Element {
-  const t = useTranslations('Home');
-
-  return (
-    <Wrapper>
-      <h1>{t('Page.title')}</h1>
-      <Link href="testpage">{t('Page.link')}</Link>
-    </Wrapper>
-  );
+  return <></>;
 }
