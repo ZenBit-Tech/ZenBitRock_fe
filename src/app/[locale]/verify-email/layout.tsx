f19ae357c47a6698dd95eb9f2d14c69b0bfb2fd2
canterbery <wotphantommail@gmail.com>
2023-11-19 12:25:22 +0200

    refactor: - translation loading

--- src/app/[locale]/verify-email/layout.tsx
+++ src/app/[locale]/verify-email/layout.tsx
@@ -1,29 +1,9 @@
-import 'server-only';
-
-import { notFound } from 'next/navigation';
-import { NextIntlClientProvider } from 'next-intl';
-import { Locale } from 'locales/i18n.config';
 import { CompactLayout } from './components';
 
 type Props = {
   children: React.ReactNode;
-  params: { locale: Locale };
 };
 
-export default async function Layout({ children, params: { locale } }: Props) {
-  let localeData;
-
-  try {
-    localeData = (await import(`locales/langs/${locale}.json`)).default;
-  } catch (error) {
-    notFound();
-  }
-
-  return (
-    <CompactLayout>
-      <NextIntlClientProvider locale={locale} messages={localeData}>
-        {children}
-      </NextIntlClientProvider>
-    </CompactLayout>
-  );
+export default function Layout({ children }: Props) {
+  return <CompactLayout>{children}</CompactLayout>;
 }
