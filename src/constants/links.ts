baf7c4a0e9dd8f92a7ebcb5aca8cf8dd630f16de
AKutusenko <76035623+AKutusenko@users.noreply.github.com>
2023-11-19 16:09:06 +0100

    Create links.ts

---
+++ src/constants/links.ts
@@ -0,0 +1,7 @@
+export const links = {
+  VERIFY_PAGE: '/verify-email',
+  SIGN_IN_PAGE: '/sign-in',
+  SIGN_UP_PAGE: '/sign-up',
+  HOME_PAGE: '/',
+  ADD_VERIFICATION_DATA: '/verification/create',
+};
