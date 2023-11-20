8ac9228ee8252f53c8e7f6fbf8fb57f9fcbf462e
canterbery <wotphantommail@gmail.com>
2023-11-20 16:21:45 +0200

    refactor: + metadata

--- src/app/[locale]/verify-email/layout.tsx
+++ src/app/[locale]/verify-email/layout.tsx
@@ -4,6 +4,10 @@ type Props = {
   children: React.ReactNode;
 };
 
+export const metadata = {
+  title: 'Verify Email',
+};
+
 export default function Layout({ children }: Props) {
   return <CompactLayout>{children}</CompactLayout>;
 }
