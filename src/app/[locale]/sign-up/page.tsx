9d7cc85cfb1c539d9f7f84c3febaba96c4e59078
canterbery <wotphantommail@gmail.com>
2023-11-24 18:55:02 +0200

    refactor: *variable name , - provider -links

--- src/app/[locale]/sign-up/page.tsx
+++ src/app/[locale]/sign-up/page.tsx
@@ -5,11 +5,10 @@ import { notFound } from 'next/navigation';
 import { Link, Box, Typography } from '@mui/material';
 import { Locale } from 'locales/i18n.config';
 import { getDictionary } from 'lib/dictionary';
-import ReduxProvider from 'store/ReduxProvider';
 import PagesContainer from 'components/PagesContainer/PagesContainer';
 import SignUpForm from 'components/SignUpForm/SignUpForm';
 import { LoadingScreen } from 'components/loading-screen';
-import { pageLinks } from 'constants/pageLinks';
+import { AppRoute } from 'enums';
 import { PolicyComponent } from 'components/PolicyComponent/PolicyComponent';
 import { SnackbarProvider } from 'components/snackbar';
 import { SignUpPageType } from 'types/auth';
@@ -19,57 +18,53 @@ type Props = {
   params: { locale: Locale };
 };
 
-export default function SignUpPage({ params: { lang } }: Props) {
+export default function SignUpPage({ params: { locale } }: Props) {
   const [data, setData] = useState<SignUpPageType | null>(null);
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const { SignUpPage } = await getDictionary('en');
-        setData(SignUpPage);
+        const { signUpPage } = await getDictionary('en');
+        setData(signUpPage);
       } catch (error) {
         notFound();
       }
     };
     fetchData();
-  }, [lang]);
+  }, [locale]);
 
   if (!data) {
     return <LoadingScreen />;
   }
 
   return (
-    <>
-      <PagesContainer>
-        <StyledBox>
-          <Typography variant="h3" sx={{ marginBottom: '1.5rem' }}>
-            {data.Main.title}
-          </Typography>
-          <SnackbarProvider>
-            <ReduxProvider>
-              <SignUpForm signUpPage={data} />
-            </ReduxProvider>
-          </SnackbarProvider>
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              flexDirection: 'column',
-              width: '90%',
-            }}
-          >
-            <SignInLink>
-              {data.Main.haveAcc}&nbsp;
-              <Link href={pageLinks.SIGN_IN_PAGE} color="primary">
-                {data.Main.signInLink}
-              </Link>
-            </SignInLink>
-            <Policy>
-              <PolicyComponent signUpPage={data} />
-            </Policy>
-          </Box>
-        </StyledBox>
-      </PagesContainer>
-    </>
+    <PagesContainer>
+      <StyledBox>
+        <Typography variant="h3" sx={{ marginBottom: '1.5rem' }}>
+          {data.Main.title}
+        </Typography>
+        <SnackbarProvider>
+          <SignUpForm SignUpPage={data} />
+        </SnackbarProvider>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            width: '90%',
+          }}
+        >
+          <SignInLink>
+            {data.Main.haveAcc}&nbsp;
+            <Link href={AppRoute.SIGN_IN_PAGE} color="primary">
+              {data.Main.signInLink}
+            </Link>
+          </SignInLink>
+          <Policy>
+            <PolicyComponent signUpPage={data} />
+          </Policy>
+        </Box>
+      </StyledBox>
+    </PagesContainer>
   );
 }
