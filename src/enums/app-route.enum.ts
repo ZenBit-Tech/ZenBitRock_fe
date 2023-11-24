2e0ecf5091eba101dda41680dad36941216bdbd3
canterbery <wotphantommail@gmail.com>
2023-11-24 18:31:57 +0200

    feat: + AppRoute , StorageKey enums

---
+++ src/enums/app-route.enum.ts
@@ -0,0 +1,13 @@
+const AppRoute = {
+  VERIFY_PAGE: '/verify-email',
+  SIGN_IN_PAGE: '/sign-in',
+  SIGN_UP_PAGE: '/sign-up',
+  HOME_PAGE: '/',
+  FORGOT_PASSWORD_PAGE: '/forgot-password',
+  TERMS_CONDITIONS: '/terms-conditions',
+  PRIVACY_POLICY: '/privacy-policy',
+  MAIN_PAGE: '/main-page',
+  ADD_VERIFICATION_DATA: '/verification/create',
+} as const;
+
+export { AppRoute };
