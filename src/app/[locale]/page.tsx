39953a1ad70c15169198f61a44aefae6eb65782d
canterbery <wotphantommail@gmail.com>
2023-11-24 18:42:29 +0200

    feat: + public route

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,11 +5,12 @@ import { useTranslations } from 'next-intl';
 import { Button, Link } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import { colors } from 'constants/colors';
-import { pageLinks } from 'constants/pageLinks';
+import { AppRoute } from 'enums';
 import { typography } from 'theme/typography';
 import { LoadingScreen } from 'components/loading-screen';
 import WelcomePageContainer from 'components/WelcomePageContainer/WelcomePageContainer';
 import { RightSection, StyledBtnWrapper } from 'components/WelcomePageContainer/styles';
+import { PublicRoute } from 'components/custom';
 
 type HomePageType = {
   Header: {
@@ -29,38 +30,40 @@ export default function HomePage() {
   }
 
   return (
-    <WelcomePageContainer>
-      <RightSection maxWidth="sm">
-        <StyledBtnWrapper>
-          <Button variant="contained" sx={{ marginRight: '10px' }} size="large">
-            <Link underline={'none'} color={colors.TEST_MAIN_COLOR} href={pageLinks.SIGN_IN_PAGE}>
-              {t('Page.signInLink')}
+    <PublicRoute>
+      <WelcomePageContainer>
+        <RightSection maxWidth="sm">
+          <StyledBtnWrapper>
+            <Button variant="contained" sx={{ marginRight: '10px' }} size="large">
+              <Link underline={'none'} color={colors.TEST_MAIN_COLOR} href={AppRoute.SIGN_IN_PAGE}>
+                {t('Page.signInLink')}
+              </Link>
+            </Button>
+            <Button variant="contained" size="large">
+              <Link underline={'none'} color={colors.TEST_MAIN_COLOR} href={AppRoute.SIGN_UP_PAGE}>
+                {t('Page.signUpLink')}
+              </Link>
+            </Button>
+          </StyledBtnWrapper>
+          <Typography
+            variant="h1"
+            align="center"
+            gutterBottom
+            sx={{
+              ...typography.h3,
+              mx: 'auto',
+            }}
+            maxWidth="500px"
+          >
+            {t('Page.title')}
+          </Typography>
+          <Button variant="contained" fullWidth size="large">
+            <Link underline={'none'} color="primary" href={AppRoute.SIGN_IN_PAGE}>
+              {t('Page.buttonTxt')}
             </Link>
           </Button>
-          <Button variant="contained" size="large">
-            <Link underline={'none'} color={colors.TEST_MAIN_COLOR} href={pageLinks.SIGN_UP_PAGE}>
-              {t('Page.signUpLink')}
-            </Link>
-          </Button>
-        </StyledBtnWrapper>
-        <Typography
-          variant="h1"
-          align="center"
-          gutterBottom
-          sx={{
-            ...typography.h3,
-            mx: 'auto',
-          }}
-          maxWidth="500px"
-        >
-          {t('Page.title')}
-        </Typography>
-        <Button variant="contained" fullWidth size="large">
-          <Link underline={'none'} color="primary" href={pageLinks.SIGN_IN_PAGE}>
-            {t('Page.buttonTxt')}
-          </Link>
-        </Button>
-      </RightSection>
-    </WelcomePageContainer>
+        </RightSection>
+      </WelcomePageContainer>
+    </PublicRoute>
   );
 }
