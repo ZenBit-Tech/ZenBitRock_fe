177bae27b3318c10a363eb15bcdebd630c60adcf
Hanna <105170538+anette1983@users.noreply.github.com>
2023-11-25 00:50:25 +0200

    Update i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,5 @@
 import { getRequestConfig } from 'next-intl/server';
 
 export default getRequestConfig(async ({ locale }) => ({
-  // eslint-disable-next-line typesafe/no-await-without-trycatch
   messages: (await import(`locales/langs/en.json`)).default,
 }));
