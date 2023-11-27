cc3c9b5cb649810c81ee4ca492611ea7f92f1229
OlgaSavina <savina5565@example.com>
2023-11-27 12:04:15 +0100

    Stay up-to-date with links in develop

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,13 +1,19 @@
 export const links = {
+ 
+  HOME_PAGE: '/',
   VERIFY_PAGE: '/verify-email',
   SIGN_IN_PAGE: '/sign-in',
   SIGN_UP_PAGE: '/sign-up',
-  HOME_PAGE: '/',
-  FORGOT_PASSWORD_PAGE: '/forgot-password',
-  TERMS_CONDITIONS: '/terms-conditions',
+  VERIFICATION_PAGE: '/verification',
+  VERIFICATION_DONE_PAGE: '/verification/done',
+  RESTORE_PASSWORD_PAGE: '/restore-password',
+  RESTORE_PASSWORD_VERIFY_CODE_PAGE: '/restore-password/verify-code',
+  RESTORE_PASSWORD_CHANGE_PASSWORD_PAGE: '/restore-password/change-password',
   PRIVACY_POLICY: '/privacy-policy',
+  TERMS_CONDITIONS: '/terms-conditions',
   MAIN_PAGE: '/main-page',
   ADD_VERIFICATION_DATA: '/verification/create',
-  RESTORE_PASSWORD_CHANGE_PASSWORD_PAGE: '/restore-password/change-password',
+  SEND_CODE_FOR_RESTORE_PASSWORD: '/email/forgot-password',
+  CONFIRM_CODE_FOR_RESTORE_PASSWORD: '/auth/confirm-email',
   RESTORE_PASSWORD_DONE_PAGE: '/restore-password/done',
 };
