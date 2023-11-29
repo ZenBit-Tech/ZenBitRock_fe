e816b0b6afb34421a3a43b70f1e40909bc1a7972
AKutusenko <76035623+AKutusenko@users.noreply.github.com>
2023-11-29 17:15:51 +0100

    fix: changed type of handle function

--- src/sections/verification-done-view/index.tsx
+++ src/sections/verification-done-view/index.tsx
@@ -13,9 +13,9 @@ export default function VerificationDoneView(): JSX.Element {
 
   const { replace } = useRouter();
 
-  function handleClick() {
+  const handleClick = () => {
     replace(AppRoute.SIGN_IN_PAGE);
-  }
+  };
 
   return (
     <Box
@@ -49,7 +49,7 @@ export default function VerificationDoneView(): JSX.Element {
             size="large"
             variant="text"
             style={{ marginBottom: '70px' }}
-            onClick={() => handleClick}
+            onClick={handleClick}
           >
             {t('submitButton')}
           </LoadingButton>
