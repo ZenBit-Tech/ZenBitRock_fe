f91d4e9cf11ce1f6431d96649a2e566015f4a432
AKutusenko <76035623+AKutusenko@users.noreply.github.com>
2023-11-29 23:24:24 +0100

    feat: created qobrix api

---
+++ src/types/qobrix-agent.ts
@@ -0,0 +1,15 @@
+export type QobrixAgentRequest = {
+  agent_type: string;
+  legacy_id: string;
+  primary_contact: string;
+};
+
+export type QobrixAgentResponse = {
+  data: {
+    data: {
+      agent_type: string;
+      legacy_id: string;
+      primary_contact: string;
+    };
+  };
+};
