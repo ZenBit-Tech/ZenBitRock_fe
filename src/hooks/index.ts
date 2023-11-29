a0f9ce8f59195f7b13c788853a46009c80822685
canterbery <wotphantommail@gmail.com>
2023-11-29 16:00:08 +0200

    feat: + hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,6 @@
 export { useEffect, useState } from 'react';
 export { useSelector } from 'react-redux';
 export { useRouter } from 'next/navigation';
+export { useOffSetTop } from './use-off-set-top';
+export { useResponsive } from './use-responsive';
+export { useTranslations } from 'next-intl';
