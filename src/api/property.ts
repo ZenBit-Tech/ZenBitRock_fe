2fd998da41e530cfba73be7de96596ade8bad46d
rudenkoserhii <rudenko.sergey@ukr.net>
2023-11-30 10:13:31 +0200

    add fetcher

--- src/api/property.ts
+++ src/api/property.ts
@@ -7,20 +7,46 @@ import { PropertyItem, PropertyList } from 'types/properties';
 
 // ----------------------------------------------------------------------
 
+const options = {
+  // mode: 'no-cors',
+  headers: {
+    'X-Api-User': 'be81403c-9488-4e21-b7b2-97189a13d485',
+    'X-Api-Key': '884b188fb86dc64c3ccb54449addd0056fc8e5fc110e4f3544a3f4c534effcee',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    Accept: 'application/json',
+  },
+};
+
 const URL = endpoints.property;
 
-export function useGetProperties() {
-  const { data, isLoading, error, isValidating } = useSWR(URL, fetcher);
+export function useGetProperties(params: {
+  page: number;
+  limit: number;
+  fields: [string];
+  media: Boolean;
+  sort: [string];
+  search: string;
+}) {
+  const { data, isLoading, error, isValidating } = useSWR(
+    [URL.list, { ...options, ...params }],
+    fetcher
+  );
 
+  if (error) {
+    console.error('Error fetching properties:', error);
+  }
   const memoizedValue = useMemo(() => {
     const properties = data?.map((property: PropertyItem) => ({
-      ...properties,
+      ...property,
       id: property.id,
       sale_rent: property.sale_rent,
       status: property.status,
       country: property.country,
       city: property.city,
       list_selling_price_amount: property.list_selling_price_amount,
+      photo: property.media[0].file.href,
     }));
 
     return {
@@ -34,30 +60,3 @@ export function useGetProperties() {
 
   return memoizedValue;
 }
-
-// ----------------------------------------------------------------------
-
-export function useGetProperty(propertyId: string) {
-  const URL = propertyId ? [endpoints.product.details, { params: { propertyId } }] : null;
-
-  const { data, isLoading, error, isValidating } = useSWR(URL, fetcher);
-
-  const memoizedValue = useMemo(() => {
-    const property = data?.map((prop: PropertyItem) => ({
-      ...property,
-      media: prop.media,
-    }));
-
-    return {
-      property: (property as PropertyItem) || [],
-      propertiesLoading: isLoading,
-      propertiesError: error,
-      propertiesValidating: isValidating,
-      propertiesEmpty: !isLoading && !data?.length,
-    };
-  }, [data, error, isLoading, isValidating]);
-
-  return memoizedValue;
-}
-
-// ----------------------------------------------------------------------
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,10 +1,9 @@
 import axios, { AxiosRequestConfig } from 'axios';
 // config
-import { HOST_API } from 'config-global';
+import { CRM_API } from 'config-global';
 
 // ----------------------------------------------------------------------
-
-const axiosInstance = axios.create({ baseURL: HOST_API });
+const axiosInstance = axios.create({ baseURL: CRM_API });
 
 axiosInstance.interceptors.response.use(
   (res) => res,
@@ -17,9 +16,7 @@ export default axiosInstance;
 
 export const fetcher = async (args: string | [string, AxiosRequestConfig]) => {
   const [url, config] = Array.isArray(args) ? args : [args];
-
   const res = await axiosInstance.get(url, { ...config });
-
   return res.data;
 };
 
