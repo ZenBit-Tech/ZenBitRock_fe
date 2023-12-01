b882ea0a6cc739bec9acab43b854a3c87fa2aa3a
Hanna <105170538+anette1983@users.noreply.github.com>
2023-12-01 14:54:50 +0200

    Update user-del-dialog.tsx

--- src/sections/user/user-del-dialog.tsx
+++ src/sections/user/user-del-dialog.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useTranslations } from 'next-intl';
-import { DialogContentText, Link, Typography } from '@mui/material';
+import { DialogContentText, Typography } from '@mui/material';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
