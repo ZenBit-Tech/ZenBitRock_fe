4c5bfc09ca65bf1b215bda64de3091f14722d9ef
rudenkoserhii <rudenko.sergey@ukr.net>
2023-12-03 21:45:48 +0200

    modify congig-globals

--- src/config-global.ts
+++ src/config-global.ts
@@ -5,6 +5,14 @@
 
 export const HOST_API = process.env.NEXT_PUBLIC_HOST_API;
 export const ASSETS_API = process.env.NEXT_PUBLIC_ASSETS_API;
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+export const QOBRIX_PROXY_URL = process.env.NEXT_PUBLIC_QOBRIX_PROXY_URL;
+export const QOBRIX_HOST = process.env.NEXT_PUBLIC_QOBRIX_HOST;
+
+export const QOBRIX_KEYS = {
+  apiKey: process.env.NEXT_PUBLIC_QOBRIX_API_KEY,
+  apiUser: process.env.NEXT_PUBLIC_QOBRIX_API_USER,
+};
 
 export const FIREBASE_API = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
