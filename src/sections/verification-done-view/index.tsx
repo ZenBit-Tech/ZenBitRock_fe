fc32c9afebb0034abde7cb36b3754ace19d033ff
AKutusenko <76035623+AKutusenko@users.noreply.github.com>
2023-12-03 20:42:52 +0100

    fix: updated verification-done page

--- src/sections/verification-done-view/index.tsx
+++ src/sections/verification-done-view/index.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useSelector } from 'react-redux';
 import { useRouter } from 'next/navigation';
 import { useTranslations } from 'next-intl';
 import Box from '@mui/material/Box';
@@ -8,83 +7,14 @@ import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import LoadingButton from '@mui/lab/LoadingButton';
 import { AppRoute } from 'enums';
-import { useCreateAgentMutation, useCreateContactMutation } from 'store/api/qobrixApi';
-import { useGetUserByIdMutation } from 'store/api/getUserApi';
-import { selectCurrentUser } from 'store/auth/authReducer';
 
 export default function VerificationDoneView(): JSX.Element {
-  const [createContact] = useCreateContactMutation();
-  const [createAgent] = useCreateAgentMutation();
-  const [getUserById] = useGetUserByIdMutation();
   const t = useTranslations('VerificationDonePage');
 
   const { replace } = useRouter();
 
-  const authState = useSelector(selectCurrentUser);
-  const userId = authState.user ? authState.user.id : null;
-
-  async function createAgentQobrix(): Promise<void> {
-    try {
-      const user = await getUserById({ id: userId });
-
-      if ('data' in user) {
-        const {
-          city,
-          country,
-          dateOfBirth,
-          email,
-          firstName,
-          id,
-          lastName,
-          nationality,
-          phone,
-          role,
-          state,
-          street,
-          zip,
-        } = user.data;
-
-        const contactData = {
-          first_name: firstName,
-          last_name: lastName,
-          role,
-          birthdate: dateOfBirth,
-          nationality,
-          street,
-          country,
-          state,
-          city,
-          post_code: zip,
-          email,
-          phone,
-          legacy_id: id,
-        };
-
-        const contact = await createContact(contactData);
-
-        if ('data' in contact) {
-          // eslint-disable-next-line @typescript-eslint/no-shadow
-          const { role, legacy_id, id } = contact.data.data;
-
-          const agentData = {
-            agent_type: role,
-            legacy_id,
-            primary_contact: id,
-          };
-
-          await createAgent(agentData);
-        }
-      }
-
-      return undefined;
-    } catch (error) {
-      return error;
-    }
-  }
-
   const handleClick = () => {
-    createAgentQobrix();
-    replace(AppRoute.SIGN_IN_PAGE);
+    replace(AppRoute.MAIN_PAGE);
   };
 
   return (
