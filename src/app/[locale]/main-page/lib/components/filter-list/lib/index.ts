d5a8deb342ec57f8c22803c3b7ae6bc60a4223c4
canterbery <wotphantommail@gmail.com>
2023-12-04 14:22:07 +0200

    feat: - prop types

--- src/app/[locale]/main-page/lib/components/filter-list/lib/index.ts
+++ src/app/[locale]/main-page/lib/components/filter-list/lib/index.ts
@@ -1,2 +1,2 @@
-export { PROPERTY_TYPES, PROPERTY_STATUS, BEDROOMS, RENT_OR_SALE } from './control-options.const';
+export { PROPERTY_STATUS, BEDROOMS, RENT_OR_SALE } from './control-options.const';
 export { FilterSchema } from './validation-schema';
