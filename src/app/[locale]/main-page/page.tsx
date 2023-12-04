b3708db2128cbf4f708e19b1875a1939e6430aed
canterbery <wotphantommail@gmail.com>
2023-12-04 12:05:21 +0200

    feat: * layout

--- src/app/[locale]/main-page/page.tsx
+++ src/app/[locale]/main-page/page.tsx
@@ -1,10 +1,19 @@
 'use client';
 
+import { Box, Typography } from '@mui/material';
 import { Navbar, ProtectedRoute } from 'components/custom';
+import { NotificationCenter, PropertyFilter } from './lib';
 
 export default function MainPage() {
   return (
     <ProtectedRoute>
+      <Box sx={{ p: '10px', margin: '0 auto', maxWidth: '800px' }}>
+        <NotificationCenter />
+        <Typography variant="h3" sx={{ marginTop: 3 }}>
+          My properties
+        </Typography>
+        <PropertyFilter />
+      </Box>
       <Navbar />
     </ProtectedRoute>
   );
