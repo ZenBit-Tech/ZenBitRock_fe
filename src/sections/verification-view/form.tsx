0bc68b640287da698c77cf40d3ce411459891bbf
AKutusenko <76035623+AKutusenko@users.noreply.github.com>
2023-12-08 22:04:09 +0100

    fix: limited minimum date entry

--- src/sections/verification-view/form.tsx
+++ src/sections/verification-view/form.tsx
@@ -307,6 +307,7 @@ export default function VerificationForm(): JSX.Element {
                     <DatePicker
                       {...field}
                       label={t('dateOfBirthPlaceholder')}
+                      minDate={new Date(1990, 0, 1)}
                       maxDate={new Date()}
                       format={datesFormats.verificationDatePicker}
                       slotProps={{
@@ -485,13 +486,13 @@ export default function VerificationForm(): JSX.Element {
 
             <LoadingButton
               fullWidth
-              color="info"
+              variant="contained"
+              color="primary"
               size="large"
               type="submit"
-              variant="outlined"
               loading={isSubmitting}
               disabled={!formFilled}
-              style={{ marginBottom: '70px' }}
+              sx={{ mb: '90px' }}
             >
               {t('submitButton')}
             </LoadingButton>
