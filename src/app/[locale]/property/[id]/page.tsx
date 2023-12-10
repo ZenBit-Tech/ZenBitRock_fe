dbdb738c81ac9e24fc986639fd40de54a619e3b8
rudenkoserhii <rudenko.sergey.v@gmail.com>
2023-12-10 22:26:48 +0200

    fix: build error

--- src/app/[locale]/property/[id]/page.tsx
+++ src/app/[locale]/property/[id]/page.tsx
@@ -4,7 +4,7 @@ import Property from 'components/custom/property';
 import { useParams } from 'next/navigation';
 
 export default function PropertyPage(): JSX.Element {
-  const {id} = useParams();
+  const { id } = useParams();
   const propertyId = Array.isArray(id) ? id[0] : id;
 
   return <Property id={propertyId} />;
