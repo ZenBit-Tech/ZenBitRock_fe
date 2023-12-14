308a483e4ba72ca6189be27f1e1c3ab0c317c9c0
rudenkoserhii <rudenko.sergey.v@gmail.com>
2023-12-14 08:54:46 +0200

    fix: pull from leads and delete leads

--- src/app/[locale]/leads/lib/components/leads-filter/leads-filter.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-function LeadsFilter(): JSX.Element {
-  return <p>LeadsFilter(for future)</p>;
-}
-
-export { LeadsFilter };
--- src/app/[locale]/leads/lib/index.ts
+++
@@ -1,2 +0,0 @@
-export { LeadsFilter } from './components/leads-filter/leads-filter';
-export { ButtonAddNewLead } from './components/button-add-new-lead/button-add-new-lead';
