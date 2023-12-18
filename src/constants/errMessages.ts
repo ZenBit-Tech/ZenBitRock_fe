66d53b05383fb34ba04c8c1472547d57fad70d61
rudenkoserhii <rudenko.sergey.v@gmail.com>
2023-12-18 14:04:15 +0200

    merge

--- src/constants/errMessages.ts
+++ src/constants/errMessages.ts
@@ -1,5 +1,5 @@
 export const errMessages = {
   SIGN_UP_ERR_MSG: 'User with this email already exists',
   SIGN_IN_ERR_MSG: 'Please enter valid credentials!',
-  DEL_USER_MSG: 'User deleted successfully!'
+  DEL_USER_MSG: 'User deleted successfully!',
 };
