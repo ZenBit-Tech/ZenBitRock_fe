6aae090b22945aa39207dd891a3648e87594003c
rudenkoserhii <rudenko.sergey.v@gmail.com>
2023-12-18 16:54:33 +0200

    feat: found enpoints

--- src/sections/lead/components/lead-history.tsx
+++ src/sections/lead/components/lead-history.tsx
@@ -1,22 +1,28 @@
-import { QobrixLead, ValueOf } from 'types';
-import { toTitleCase } from 'utils';
-import { Grid, Typography } from '@mui/material';
-import { useTranslations } from 'hooks';
+import { useEffect, useTranslations } from 'hooks';
 
 type Props = { lead: QobrixLead };
 
 const LeadHistorySection = ({ lead }: Props) => {
   const { id, created } = lead;
-  ('https://trial5251.eu1.qobrix.com/api/v2/sms-messages/related-with/RelatedOpportunityOpportunities/e52f7f35-b3fd-488f-a13f-aa00c4c15177/');
-  ('https://trial5251.eu1.qobrix.com/api/v2/email-messages/related-with/RelatedOpportunityOpportunities/e52f7f35-b3fd-488f-a13f-aa00c4c15177/');
-  ('https://trial5251.eu1.qobrix.com/api/v2/tasks/related-with/RelatedOpportunityOpportunities/e52f7f35-b3fd-488f-a13f-aa00c4c15177/');
-  ('https://trial5251.eu1.qobrix.com/api/v2/workflows/taskWorkflowId/stages');
-  ('https://trial5251.eu1.qobrix.com/api/v2/meetings/related-with/RelatedOpportunityOpportunities/e52f7f35-b3fd-488f-a13f-aa00c4c15177/');
-  ('https://trial5251.eu1.qobrix.com/api/v2/calls/related-with/RelatedOpportunityOpportunities/e52f7f35-b3fd-488f-a13f-aa00c4c15177/');
-  ('https://trial5251.eu1.qobrix.com/api/v2/opportunities/related-with/ConversionStatusWorkflowStages/e52f7f35-b3fd-488f-a13f-aa00c4c15177/');
-  ('https://trial5251.eu1.qobrix.com/api/v2/workflows?search=resource%20%3D%3D%20%22Opportunities%22');
-  `search = resource == 'Opportunities'`;
+  ('sms https://trial5251.eu1.qobrix.com/api/v2/sms-messages/related-with/RelatedOpportunityOpportunities/e52f7f35-b3fd-488f-a13f-aa00c4c15177/');
+  ('emails https://trial5251.eu1.qobrix.com/api/v2/email-messages/related-with/RelatedOpportunityOpportunities/e52f7f35-b3fd-488f-a13f-aa00c4c15177/');
+  ('tasks https://trial5251.eu1.qobrix.com/api/v2/tasks/related-with/RelatedOpportunityOpportunities/e52f7f35-b3fd-488f-a13f-aa00c4c15177/');
+  ('tasks by id https://trial5251.eu1.qobrix.com/api/v2/workflows/taskWorkflowId/stages');
+  ('meetings https://trial5251.eu1.qobrix.com/api/v2/meetings/related-with/RelatedOpportunityOpportunities/e52f7f35-b3fd-488f-a13f-aa00c4c15177/');
+  ('calls https://trial5251.eu1.qobrix.com/api/v2/calls/related-with/RelatedOpportunityOpportunities/e52f7f35-b3fd-488f-a13f-aa00c4c15177/');
+  ('statuses https://trial5251.eu1.qobrix.com/api/v2/opportunities/e52f7f35-b3fd-488f-a13f-aa00c4c15177/changes');
+
   const t = useTranslations('leadDetailsPage');
+
+  useEffect(() => {
+    const smses = useGetSmsesQuery(id);
+    const emails = useGetEmailsQuery(id);
+    const meetings = useGetMeetingsQuery(id);
+    const calls = useGetCallsQuery(id);
+    const tasks = useGetTasksQuery(id);
+    tasks.forEach((task) => useGetTasksByLeadQuery(task.id));
+    const statuses = useGetStatusesQuery(id);
+  }, []);
 
   return (
     <>
