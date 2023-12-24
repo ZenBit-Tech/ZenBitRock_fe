83204477f25989b8e65a892d968fd19398cb820f
rudenkoserhii <rudenko.sergey.v@gmail.com>
2023-12-24 15:24:00 +0200

    feat: add property-card

---
+++ src/components/custom/property-card/property-card.tsx
@@ -0,0 +1,127 @@
+'use client';
+
+import { Box, Card } from '@mui/material';
+import { QobrixProperty } from 'types/qobrix';
+import Iconify from 'components/iconify';
+import Image from 'components/image';
+import { useRouter, useTranslations } from 'hooks';
+import { QOBRIX_HOST } from 'config-global';
+import { backgroundImages } from 'constants/backgroundImgLinks';
+import { AppRoute } from 'enums';
+import { getCountries } from 'sections/verification-view/drop-box-data';
+import { fCurrency } from 'utils/format-number';
+import {
+  BoxStyled,
+  CardMediaStyled,
+  LinkStyled,
+  TextMiddleStyled,
+  TextStyled,
+  TypographyStyled,
+} from './styles';
+
+type Props = {
+  property: QobrixProperty;
+};
+
+const PropertyCard = ({ property }: Props) => {
+  const t = useTranslations('properties');
+  const router = useRouter();
+
+  const { id, saleRent, status, country, city, price, photo } = property;
+
+  return (
+    <Card
+      key={id}
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '45%',
+        marginBottom: '2rem',
+      }}
+    >
+      <Box
+        sx={{
+          position: 'relative',
+          width: '100%',
+        }}
+      >
+        <CardMediaStyled
+          sx={{
+            width: '100%',
+            objectFit: 'cover',
+            overflow: 'hidden',
+          }}
+        >
+          <Image
+            src={photo ? `${QOBRIX_HOST}${photo}` : backgroundImages.BG_PROPERTIES_PAGE}
+            alt={t('alt')}
+            sx={{
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+            }}
+          />
+        </CardMediaStyled>
+        <TextStyled
+          sx={{
+            position: 'absolute',
+            bottom: '1rem',
+            right: '1rem',
+            fontWeight: 'bold',
+            color: 'white',
+            textShadow: '1px 1px 2px black',
+          }}
+        >
+          {fCurrency(Number(price))}
+        </TextStyled>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          padding: '1rem',
+          width: '100%',
+          height: 'calc(100% - 100px)',
+        }}
+      >
+        <BoxStyled
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: '100%',
+          }}
+        >
+          <TextStyled
+            sx={{
+              fontWeight: 'bold',
+            }}
+          >
+            {t(saleRent)}
+          </TextStyled>
+          <TextStyled
+            sx={{
+              fontWeight: 'bold',
+            }}
+          >
+            {t(status)}
+          </TextStyled>
+        </BoxStyled>
+        <TextMiddleStyled>
+          {getCountries().find((object) => object.value === country)?.label}, {city}
+        </TextMiddleStyled>
+        <LinkStyled
+          sx={{ padding: '14px', marginTop: 'auto' }}
+          variant="contained"
+          color="primary"
+          onClick={() => router.push(`${AppRoute.PROPERTY_PAGE}/${id}`)}
+        >
+          <TypographyStyled>{t('Description')}</TypographyStyled>
+          <Iconify icon="ri:arrow-right-s-line" height="auto" />
+        </LinkStyled>
+      </Box>
+    </Card>
+  );
+};
+
+export { PropertyCard };
