8aa22bbbee59cf46d2e925ae51989b97a13042dc
rudenkoserhii <rudenko.sergey.v@gmail.com>
2024-01-03 20:36:50 +0200

    fix: hide notification center

--- src/app/[locale]/main-page/lib/components/notification-center/notification-center.tsx
+++ src/app/[locale]/main-page/lib/components/notification-center/notification-center.tsx
@@ -3,28 +3,37 @@
 import { Box, Typography } from '@mui/material';
 import { CustomLink } from 'components/custom';
 import Iconify from 'components/iconify';
+import { useState } from 'hooks';
 
-const NotificationCenter = (): JSX.Element => (
-  <Box>
-    <Typography>Notification center</Typography>
-    <Box
-      sx={{
-        p: 1,
-        marginTop: '5px',
-        borderBottom: '1px solid grey',
-        display: 'flex',
-        justifyContent: 'space-between',
-      }}
-    >
-      <Typography>No new notifications</Typography>
-      <CustomLink href="#">
-        <Box component="span" display="flex" alignItems="center">
-          View
-          <Iconify icon="mingcute:right-line" width="15px" height="15x" color="black" />
+const NotificationCenter = (): JSX.Element => {
+  const [isVisible] = useState<boolean>(false);
+
+  return (
+    <>
+      {isVisible && (
+        <Box>
+          <Typography>Notification center</Typography>
+          <Box
+            sx={{
+              p: 1,
+              marginTop: '5px',
+              borderBottom: '1px solid grey',
+              display: 'flex',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Typography>No new notifications</Typography>
+            <CustomLink href="#">
+              <Box component="span" display="flex" alignItems="center">
+                View
+                <Iconify icon="mingcute:right-line" width="15px" height="15x" color="black" />
+              </Box>
+            </CustomLink>
+          </Box>
         </Box>
-      </CustomLink>
-    </Box>
-  </Box>
-);
+      )}
+    </>
+  );
+};
 
 export { NotificationCenter };
