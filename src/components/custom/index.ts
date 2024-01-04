709e4da9601355ae72c8bb3dbba97a7936757862
canterbery <wotphantommail@gmail.com>
2024-01-04 10:45:07 +0200

    feat: + page title component

--- src/components/custom/index.ts
+++ src/components/custom/index.ts
@@ -10,3 +10,4 @@ export { PropertyCard } from './propery-card/property-card';
 export { ChatMessageItem } from './chat-message-item';
 export { MockChatMessageItem } from './mock-chat-message-item';
 export { NoDataFound } from './no-data-found/no-data-found';
+export { GoBackPageTitile } from './go-back-page-title/go-back-page-title';
