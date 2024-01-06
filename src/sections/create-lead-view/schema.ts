8dad2b7ef1924815c83ab4de0d78a4a6ccf93f32
AKutusenko <76035623+AKutusenko@users.noreply.github.com>
2024-01-06 20:47:40 +0100

    fix: added not equal check

--- src/sections/create-lead-view/schema.ts
+++ src/sections/create-lead-view/schema.ts
@@ -31,15 +31,19 @@ export const FormSchema = Yup.object().shape<Shape<ICreateLeadData>>({
   totalAreaTo: Yup.number().min(0, 'Only positive value').max(10000, 'Max 10000'),
   priceRangeRentFrom: Yup.number()
     .min(100, 'Min 100')
-    .lessThan(Yup.ref('priceRangeRentTo'), 'Should be less than max value'),
+    .lessThan(Yup.ref('priceRangeRentTo'), 'Should be less than max value')
+    .notOneOf([Yup.ref('priceRangeRentTo')], 'Values should not be equal'),
   priceRangeRentTo: Yup.number()
     .max(10000, 'Max 10000')
-    .moreThan(Yup.ref('priceRangeRentFrom'), 'Should be more than min value'),
+    .moreThan(Yup.ref('priceRangeRentFrom'), 'Should be more than min value')
+    .notOneOf([Yup.ref('priceRangeRentFrom')], 'Values should not be equal'),
   priceRangeBuyFrom: Yup.number()
     .min(10000, 'Min 10000')
-    .lessThan(Yup.ref('priceRangeBuyTo'), 'Should be less than max value'),
+    .lessThan(Yup.ref('priceRangeBuyTo'), 'Should be less than max value')
+    .notOneOf([Yup.ref('priceRangeBuyTo')], 'Values should not be equal'),
   priceRangeBuyTo: Yup.number()
     .max(10000000, 'Max 10000000')
-    .moreThan(Yup.ref('priceRangeBuyFrom'), 'Should be more than min value'),
+    .moreThan(Yup.ref('priceRangeBuyFrom'), 'Should be more than min value')
+    .notOneOf([Yup.ref('priceRangeBuyFrom')], 'Values should not be equal'),
   locations: Yup.mixed<QobrixLocations>().nullable(),
 });
