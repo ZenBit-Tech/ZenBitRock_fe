e7ab141896fc9f1c3cae4244a704e2f8ad09c5a0
rudenkoserhii <rudenko.sergey.v@gmail.com>
2024-01-11 19:18:46 +0200

    change header avatar sizes and add hover

--- src/components/custom/header/lib/header-avatar.tsx
+++ src/components/custom/header/lib/header-avatar.tsx
@@ -1,5 +1,6 @@
 import Avatar from '@mui/material/Avatar';
 import { AvatarShape } from 'assets/illustrations';
+import { colors } from 'constants/colors';
 import { useTranslations } from 'hooks';
 
 type Props = {
@@ -15,15 +16,20 @@ const HeaderAvatar = ({ avatar }: Props): JSX.Element => {
       alt={t('avatar')}
       sx={{
         mx: 'auto',
-        width: { xs: 48, md: 64 },
-        height: { xs: 48, md: 64 },
+        width: '3rem',
+        height: '3rem',
         zIndex: 10,
+        transition: 'all 200ms ease-out',
+        '&:hover': {
+          backgroundColor: colors.BUTTON_SECOND_COLOR,
+          transition: 'all 200ms ease-out',
+        },
       }}
     >
       <AvatarShape
         sx={{
-          width: { xs: 36, md: 48 },
-          height: { xs: 36, md: 48 },
+          width: 36,
+          height: 36,
         }}
       />
     </Avatar>
