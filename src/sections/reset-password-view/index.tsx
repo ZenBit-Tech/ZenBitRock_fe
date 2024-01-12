52b5d38a08e232ae1a808510810d65de957bad14
OlgaSavina <savina5565@example.com>
2024-01-12 13:12:34 +0200

    fix: check imports

--- src/sections/reset-password-view/index.tsx
+++ src/sections/reset-password-view/index.tsx
@@ -1,21 +1,19 @@
 'use client';
 
-import ReduxProvider from 'store/ReduxProvider';
-import { useMockedUser } from 'hooks/use-mocked-user';
-import { SnackbarProvider } from 'components/snackbar';
-import CustomBreadcrumbs from 'components/custom-breadcrumbs';
+import { Box } from '@mui/material';
 import Container from '@mui/material/Container';
+import ReduxProvider from 'store/ReduxProvider';
+import { SnackbarProvider } from 'components/snackbar';
 import { useSettingsContext } from 'components/settings';
 import { useTranslations } from 'next-intl';
-import { paths } from 'routes/paths';
-import Form from './form';
 import { GoBackPageTitile } from 'components/custom';
-import { Box } from '@mui/material';
+import Form from './form';
+
 
 export default function ResetPasswordView(): JSX.Element {
   const settings = useSettingsContext();
   const t = useTranslations('ResetPasswordPage');
-  const { user } = useMockedUser();
+
 
   return (
     <Container maxWidth={settings.themeStretch ? false : 'lg'} sx={{ paddingTop: '1rem' }}>
--- src/sections/verify-old-password-view/index.tsx
+++ src/sections/verify-old-password-view/index.tsx
@@ -1,16 +1,15 @@
 'use client';
 
+import { useTranslations } from 'next-intl';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import ReduxProvider from 'store/ReduxProvider';
 import { SnackbarProvider } from 'components/snackbar';
 import { useSettingsContext } from 'components/settings';
-import { useTranslations } from 'next-intl';
-import Container from '@mui/material/Container';
-import CustomBreadcrumbs from 'components/custom-breadcrumbs';
 import { useMockedUser } from 'hooks/use-mocked-user';
-import { paths } from 'routes/paths';
-import Form from './form';
 import { GoBackPageTitile } from 'components/custom';
-import { Box } from '@mui/material';
+import Form from './form';
+
 
 export default function VerifyOldPasswordView(): JSX.Element {
   const settings = useSettingsContext();
