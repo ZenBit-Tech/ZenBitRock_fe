d2443f38e06212cdc1369f4a4a08aba61c57a223
AKutusenko <76035623+AKutusenko@users.noreply.github.com>
2024-01-15 17:25:14 +0100

    fix: changed logo style

--- src/components/custom/header/styles.ts
+++ src/components/custom/header/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { colors } from 'constants/colors';
 
 export const Logo = styled.a`
-  color: ${colors.TEST_MAIN_COLOR};
-  font-size: 25px;
+  color: ${colors.PRIMARY_DARK_COLOR};
+  font-size: 28px;
+  font-weight: 700;
 `;
