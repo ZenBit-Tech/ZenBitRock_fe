950920fc62d195280903f00d0b4471361a92e5ec
rudenkoserhii <rudenko.sergey.v@gmail.com>
2024-01-16 20:15:41 +0200

    fix: remove comments

--- src/sections/chat/components/chat-info/form-add-agents.tsx
+++ src/sections/chat/components/chat-info/form-add-agents.tsx
@@ -148,7 +148,6 @@ export function FormAddAgents({
         }}
         inputValue={inputValue}
         onInputChange={(event, newInputValue, reason) => {
-          // debugger;
           if (reason === 'reset') {
             setInputValue('');
 
