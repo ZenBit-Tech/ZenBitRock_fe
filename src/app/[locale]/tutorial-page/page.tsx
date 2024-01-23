c4ca9f95818d6be48160abaf4a9fd461e400b548
OlgaSavina <savina5565@example.com>
2024-01-23 13:49:02 +0200

    feat: tutorial page

---
+++ src/app/[locale]/tutorial-page/page.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { useTranslations } from 'next-intl';
+import { useRouter } from 'next/navigation';
+import { Box, Button, Container, Stack, Typography } from '@mui/material';
+import { MotivationIllustration } from 'assets/illustrations';
+import { useSettingsContext } from 'components/settings';
+import { AppRoute } from 'enums';
+
+export default function TutorialPage(): JSX.Element {
+  const t = useTranslations('tutorialPage');
+  const settings = useSettingsContext();
+  const router = useRouter();
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: '100vh',
+      }}
+    >
+      <Container maxWidth={settings.themeStretch ? false : 'lg'} sx={{ pb: 14, marginTop: '20px' }}>
+        <Box sx={{ mb: 2 }}>
+          <Typography variant="h4" textAlign="center" fontWeight="fontWeightRegular">
+            {t('welcomeTxt')}
+          </Typography>
+          <Typography variant="h4" textAlign="center" fontWeight="fontWeightRegular">
+            {t('welcomeSubTxt')}
+          </Typography>
+        </Box>
+        <Stack
+          alignItems="center"
+          justifyContent="center"
+          gap="1"
+          padding="2"
+          maxWidth="70%"
+          margin="0 auto"
+        >
+          <MotivationIllustration />
+
+          <Typography textAlign="center"> {t('select')}</Typography>
+
+          <Button
+            type="submit"
+            variant="contained"
+            fullWidth
+            sx={{
+              mb: '5px',
+              mt: '20px',
+              padding: '14px',
+            }}
+            color="primary"
+            onClick={() => router.push(AppRoute.QUICK_START_GUIDE_PAGE)}
+          >
+            {t('start')}
+          </Button>
+        </Stack>
+      </Container>
+    </Box>
+  );
+}
