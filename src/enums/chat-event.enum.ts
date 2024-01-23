5f741fd617a6df6c591092a42aed8856e60d795a
AKutusenko <76035623+AKutusenko@users.noreply.github.com>
2024-01-23 14:34:28 +0100

    feat: added emits

--- src/enums/chat-event.enum.ts
+++ src/enums/chat-event.enum.ts
@@ -4,6 +4,8 @@ const ChatEvent = {
   RequestAllChats: 'request_all_chats',
   RequestUnreadMessages: 'request_unread_messages',
   NewChat: 'new_chat',
+  ChatDeleted: 'chat_deleted',
+  ChatUpdated: 'chat_updated',
 } as const;
 
 export { ChatEvent };
