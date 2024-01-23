251bf1cbf867b645661ff4ce796a43d43ded0b78
AKutusenko <76035623+AKutusenko@users.noreply.github.com>
2024-01-23 14:37:16 +0100

    fix: changed chats page

--- src/sections/chats-view/chats-list.tsx
+++ src/sections/chats-view/chats-list.tsx
@@ -1,67 +1,78 @@
-import { useState, useCallback } from 'react';
 import { Box, Fab, Stack, TextField, Typography } from '@mui/material';
-import { useScrollToTop } from 'hooks';
-import { Chat } from 'types';
-import { NoDataFound } from 'components/custom';
+import { useScrollToTop, useState, useCallback, useInfinityScroll } from 'hooks';
+import { NoDataFound, chatsMockData, useOnboardingContext } from 'components/custom';
+import { Page500 } from 'sections/error';
+import { LoadingScreen } from 'components/loading-screen';
+import { useGetChatsQuery } from 'store/chat/chat-api';
 import ChatItem from './chat-item';
 import { Values, getSortOptions } from './helpers/drop-box-data';
-import { sortChats } from './helpers';
 import { SortComponent } from './sort-component';
 
 type Props = {
-  chats: Chat[];
   t: Function;
 };
 
-export default function ChatsList({ chats, t }: Props) {
-  const [searchTerm, setSearchTerm] = useState<string>('');
+const FIRST_PAGE: number = 1;
+
+export default function ChatsList({ t }: Props) {
+  const [page, setPage] = useState<number>(FIRST_PAGE);
+  const [searchParam, setSearchParam] = useState<string>('');
   const [sortBy, setSortBy] = useState<Values>(getSortOptions(t)[0]);
+
+  const {
+    state: { tourActive },
+  } = useOnboardingContext();
+
+  const { data, isLoading, isError, isFetching } = useGetChatsQuery({
+    page,
+    limit: 100,
+    sortType: sortBy.value,
+    searchParam,
+  });
 
   const isVisible = useScrollToTop();
 
   const handleSearchChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(event.target.value);
+    setSearchParam(event.target.value);
   }, []);
 
   const scrollToTop = (): void => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
-  const filteredChats = chats.filter(
-    (chat) =>
-      chat.title?.toLowerCase().startsWith(searchTerm.toLowerCase()) ||
-      chat.members.some(
-        (member) =>
-          member.firstName.toLowerCase().startsWith(searchTerm.toLowerCase()) ||
-          member.lastName.toLowerCase().startsWith(searchTerm.toLowerCase())
-      )
-  );
-
-  const sortedChats = sortChats(filteredChats, sortBy.value);
-
   const handleSortBy = useCallback((newValue: Values) => {
     setSortBy(newValue);
   }, []);
 
+  const chats = tourActive ? chatsMockData.data : data || [];
+
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
+
+  if (isError) {
+    return <Page500 />;
+  }
+
   return (
     <Box display="flex" flexDirection="column">
       <TextField
-        value={searchTerm}
+        value={searchParam}
         onChange={handleSearchChange}
         placeholder={t('searchPlaceholder')}
       />
 
       <SortComponent t={t} sort={sortBy} onSort={handleSortBy} sortOptions={getSortOptions(t)} />
 
-      {searchTerm && sortedChats.length ? (
+      {searchParam && chats.length ? (
         <Typography textAlign="center" variant="h6" sx={{ my: 1 }}>
-          {`${t('results')} ${searchTerm}:`}
+          {`${t('results')} ${searchParam}:`}
         </Typography>
       ) : null}
 
-      {sortedChats.length ? (
+      {chats.length ? (
         <Stack>
-          {sortedChats.map((chat, idx) => (
+          {chats.map((chat, idx) => (
             <ChatItem key={chat.id} chat={chat} className={idx === 0 ? 'onboarding-step-14' : ''} />
           ))}
 
--- src/sections/chats-view/index.tsx
+++ src/sections/chats-view/index.tsx
@@ -1,18 +1,8 @@
 import { Backdrop, CircularProgress, Container } from '@mui/material';
 import { Stack } from '@mui/system';
 import { useTranslations } from 'next-intl';
-import { useSelector, useMount, useState } from 'hooks';
-import { selectCurrentUser } from 'store/auth/authReducer';
-import { useGetChatsQuery } from 'store/chat/chat-api';
-import { LoadingScreen } from 'components/loading-screen';
-import {
-  GoBackPageTitile,
-  Onboarding,
-  useOnboardingContext,
-  DELAY,
-  chatsMockData,
-} from 'components/custom';
-import { Page500 } from 'sections/error';
+import { useMount, useState } from 'hooks';
+import { GoBackPageTitile, Onboarding, useOnboardingContext, DELAY } from 'components/custom';
 import ChatsList from './chats-list';
 import AddGroupChatButton from './add-group-chat-button';
 
@@ -32,21 +22,6 @@ export default function ChatsView(): JSX.Element {
       }, DELAY);
     }
   });
-
-  const authState = useSelector(selectCurrentUser);
-  const userId = authState.user ? authState.user.id : '';
-
-  const { data, isLoading, isError } = useGetChatsQuery({ userId });
-
-  const chats = tourActive ? chatsMockData : data;
-
-  if (isLoading) {
-    return <LoadingScreen />;
-  }
-
-  if (isError) {
-    return <Page500 />;
-  }
 
   return (
     <Container sx={{ pb: 8, pt: 0, px: 2 }} className="onboarding-step-12">
@@ -69,7 +44,7 @@ export default function ChatsView(): JSX.Element {
 
         <AddGroupChatButton t={t} />
       </Stack>
-      {chats && <ChatsList t={t} chats={chats} />}
+      <ChatsList t={t} />
     </Container>
   );
 }
